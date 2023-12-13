6e87d57b9f8a5a9256a463ef26880b2b2b6e0821
Aman Kushwah <amank@tech-radix.com>
2023-12-13 15:08:57 +0530

    Practice.

--- src/JavaScript/Problem4.js
+++ src/JavaScript/Problem4.js
@@ -3,9 +3,38 @@ import React from "react";
 function Problem4() {
   const handleProblem = () => {
     // console.log('Aman\"'.length);
-    let str = "Please give Rs 100";
-    let amount = str.slice(15);
-    console.log(amount);
+    // let str = "Please give Rs 100";
+    // let amount = str.slice(15);
+    // console.log(amount);
+
+    let num = [1, 2, 3, 4];
+    // num.forEach((element) => {
+    //   console.log(element * element);
+    // });
+
+    // for (let i of num) {
+    //   console.log(i);
+    // }
+
+    // for (let i in num) {
+    //   console.log(i);
+    // }
+
+    // num.map((value, index, arr) => {
+    //   console.log(value);
+    //   console.log(index);
+    //   console.log(arr);
+    // });
+
+    // let a = num.filter((val) => {
+    //   return val > 2;
+    // });
+    // console.log(a);
+
+    let b = num.reduce((a, b) => {
+      return a + b;
+    });
+    console.log(b);
   };
 
   return (
